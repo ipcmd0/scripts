ed32eb17de58e2aaf9b479a3b5409137effbed21
ipcmd0 <kayvan_romaezi@yahoo.com>
2013-01-31 21:35:30 -0500

    Changed code to take advantage of REST API

--- paloalto_rule.py
+++ paloalto_rule.py
@@ -4,61 +4,47 @@ import xml.etree.ElementTree as xml
 from xml.dom import minidom
 import sys
 
-# Function that creates the base structure of the xml file
-def make_tree(nodes):
-    root_node = xml.Element(nodes.pop(0))
-    parent_node = root_node
+#Creates the xml structure of a single policy
+def policy_structure(nodes):
+    root_node = xml.Element("root")
+    root_node.attrib['name'] = rulename
 
     for node in nodes:
-        element = xml.SubElement(parent_node, node)
-        parent_node = element
+        element = xml.SubElement(root_node, node)
+        if node == "option":
+            option = xml.SubElement(element, "disable-server-response-inspection")
+        if node == "profile-setting":
+            group = xml.SubElement(element, "group")
+
     return root_node
-
-#Creates the xml structure of a single policy
-def policy_structure(base, nodes):
-    for rulebase in base.findall(".//rules"):
-        entry = xml.SubElement(rulebase, "entry")
-
-        for node in nodes:
-            element = xml.SubElement(entry, node)
-
-            if node == "option":
-                option = xml.SubElement(element, "disable-server-response-inspection")
-                option.text = "no"
-            if node == "profile-setting":
-                group = xml.SubElement(element, "group")
-    return base
-
-#function to name firewall rule
-def rule(base, name):
-    for rulebase in base.findall('.//rules/entry'):
-        rulebase.attrib['name'] = name
-    return base
 
 #function to populate the firewall rule with generic information
 def definepolicy(base, node, text):
-    for rulebase in base.findall(".//rules/entry/" + node):
+    for rulebase in base.findall(".//" + node):
         member = xml.SubElement(rulebase, "member")
         member.text = text
     return base
 
 #lists used to feed data to the xml body structure
-nodelist = ['config', 'devices', 'entry', 'vsys', 'entry', 'rulebase', 'security', 'rules'] 
-policylist = ['option', 'from', 'to', 'source', 'destination', 'source-user', 'category', 'application', 'service', 'hip-profiles', 'log-start', 'log-end', 'log-setting', 'negate-source', 'negate-destination', 'action', 'profile-setting']
+nodelist = ['option', 'from', 'to', 'source', 'destination', 'source-user', 'category', 'application', 'service', 'hip-profiles', 'log-start', 'log-end', 'log-setting', 'negate-source', 'negate-destination', 'action', 'profile-setting', 'description']
 genericsettings = ['source-user', 'category', 'application', 'hip-profiles']
 
 
-config = make_tree(nodelist)
 
 #Generic variables for testing, will be replaced with input from file information
-name = "test"
+rulename = "test"
 srcip = "1.1.1.1"
 dstip = "2.2.2.2"
 action = "allow"
 service = "tcp8080"
+description = "test"
+srczone = "Trust"
+dstzone = "Untrust"
 
-config = policy_structure(config, policylist)
-config = rule(config, name)
+#xpathvalue = /config/devices/entry/vsys/entry/rulebase/security/rules/entry[@name=rulename]
+
+config = policy_structure(nodelist)
+#config = rule(config, name)
 
 #dynamic information
 config = definepolicy(config, "from", "Untrust")
@@ -77,4 +63,4 @@ config = definepolicy(config, "negate-destination", "no")
 for item in genericsettings:
     config = definepolicy(config, item, "any")
 
-print minidom.parseString(xml.tostring(config)).toprettyxml(indent = "   ")
+print xml.tostring(config)
